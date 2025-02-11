4e2370839d6262fe75830c661d68d97d0c5cf0ec
JongDeug <jongdeug2021@gmail.com>
2025-02-12 04:40:42 +0900

    CI/CD: 배포

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images: { domains: ["jongdeug.port0.org", "localhost"] }
+    images: {
+        domains: ["jongdeug.port0.org", "localhost"]
+    },
+    experimental: {
+        optimizeCss: true,
+    }
 };
 
 export default nextConfig;
