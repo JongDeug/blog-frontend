72540c2f0565bd032670a92e8072070a078d2f58
JongDeug <jongdeug2021@gmail.com>
2025-02-13 13:13:12 +0900

    FEAT: rss 구독

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -2,11 +2,11 @@ import { env } from "@/const/env";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import Link from "next/link";
+import RSSBadges from "@/components/rss-badges";
+import { rssList } from "@/lib/rss-list";
 
-async function getRSSPosts() {
-  const response = await fetch(`${env.URL}/api/next/rss`, {
-    cache: "no-store",
-  });
+async function getRSSPosts(queryString: string) {
+  const response = await fetch(`${env.API_URL}/common/feed?${queryString}`);
   const resOrError = await response.json();
 
   if (!response.ok) {
@@ -16,14 +16,25 @@ async function getRSSPosts() {
   return resOrError;
 }
 
-export default async function RSSFeed() {
-  const posts = await getRSSPosts();
+export default async function RSSFeed({
+  searchParams,
+}: {
+  searchParams: Promise<{ url: string }>;
+}) {
+  let { url } = await searchParams;
+  if (!url) url = rssList[0]?.url;
 
+  const queryString = new URLSearchParams();
+  queryString.append("url", url);
+
+  const posts = await getRSSPosts(queryString.toString());
   return (
     <div className="flex flex-col gap-10">
       <section className="flex gap-2 w-fit tracking-tighter items-center transition-all duration-200 hover:scale-105 hover:text-green-500">
         <h1 className="text-3xl font-bold">RSS 구독</h1>
       </section>
+
+      <RSSBadges />
 
       <section>
         <ul className="space-y-4">
@@ -48,7 +59,6 @@ export default async function RSSFeed() {
                 <p className="text-gray-400 text-sm">
                   {format(post.pubDate, "PPP EEE p", { locale: ko })}
                 </p>
-                <p>{post.source}</p>
               </li>
             )
           )}
---
+++ src/lib/rss-list.ts
@@ -0,0 +1,42 @@
+export const rssList = [
+  {
+    name: "인프런",
+    url: "https://tech.inflab.com/rss.xml",
+  },
+  {
+    name: "GeekNews",
+    url: "https://feeds.feedburner.com/geeknews-feed",
+  },
+  {
+    name: "원티드",
+    url: "https://medium.com/feed/wantedjobs",
+  },
+  {
+    name: "당근",
+    url: "https://medium.com/feed/daangn",
+  },
+  {
+    name: "무신사",
+    url: "https://medium.com/feed/musinsa-tech",
+  },
+  {
+    name: "카카오",
+    url: "https://tech.kakao.com/feed/",
+  },
+  {
+    name: "토스",
+    url: "https://toss.tech/rss.xml",
+  },
+  {
+    name: "직방",
+    url: "https://medium.com/feed/zigbang",
+  },
+  {
+    name: "소카",
+    url: "https://tech.socarcorp.kr/feed",
+  },
+  {
+    name: "여기어때",
+    url: "https://techblog.gccompany.co.kr/feed",
+  },
+];
