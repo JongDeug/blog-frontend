6dd6f5835fc10da368a4f343d712f2bfb6ec9943
JongDeug <jongdeug2021@gmail.com>
2025-02-14 16:50:38 +0900

    FIX: domain 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,11 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ["jongdeug.port0.org", "localhost"]
-    },
-    images: {
         remotePatterns: process.env.NODE_ENV === "production" ?
             [
                 {
                     protocol: 'https',
-                    hostname: 'jongdeug.port0.org',
+                    hostname: 'jongdeug.ddns.net',
                 },
             ] :
             [
