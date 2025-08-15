9aefb5dc1f52f88fad002ef4c8a2d16479051dc8
JongDeug <jongdeug2021@gmail.com>
2025-08-16 00:11:27 +0900

    chore: 홈화면 글 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,14 @@ export default function Home() {
       <section>
         <h2 className="text-2xl font-semibold mb-2">꾸벅 🙇‍♂️</h2>
         <p className="whitespace-pre-line">
-          {`안녕하세요! ✋ 이곳은 제가 학습한 내용들을 정리하고 공유하기 위해 만든 공간입니다.
-제가 작성한 글이 함께 공부하는 분들에게도 도움이 됐으면 좋겠습니다. 여러분들의 소중한 피드백은 제가 성장하는 데 큰 힘이 됩니다.`}
+          {`안녕하세요! ✋ 이곳은 제가 개발자로써 경험하거나 학습한 내용들을 정리하기 위해 만든 공간입니다. 제가 작성한 글이 누군가에게 도움이 된다면 더할 나위 없을 것 같습니다:)`}
         </p>
       </section>
 
       <section>
         <h2 className="text-2xl font-semibold mb-2">목표</h2>
         <p className="whitespace-pre-line">
-          {`프로그래밍은 가치를 만들어내는 도구라고 생각합니다. 도구를 활용해서 맛있는 피자 한 판을 만들어 팔 줄 아는 개발자가 되는 것이 목표입니다.🔥`}
+          {`프로그래밍은 가치를 만들어내는 도구라고 생각합니다. 도구를 잘 활용해서 가치를 만들어낼줄 아는 개발자가 될 것 입니다! 🔥`}
         </p>
       </section>
       <section className="tracking-tighter text-neutral-500 text-sm">
